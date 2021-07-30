fec773e2dd8f49417c42a3610b0a690e2d2ce48c
jesusmengibardelom96 <jesusmengibardelom96@gmail.com>
2021-07-30 13:00:41 +0200

    Setting includes on index

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <?php include "includes/head.php"; ?>
+    </head>
+    <body>
+        <?php include "includes/header.php"; ?>
+        <?php include "includes/main.php"; ?>
+        <?php include "includes/footer.php";?>
+    </body>
+</html>
