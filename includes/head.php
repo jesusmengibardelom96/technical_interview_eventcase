24bbae1eafb56953fa9de324ee6f21ae14036ed0
jesusmengibardelom96 <jesusmengibardelom96@gmail.com>
2021-07-30 13:15:20 +0200

    Setting up my css

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,6 @@
 <!-- CSS links -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="../css/style.css" rel="stylesheet">
 
 <!-- JS links -->
 <script src="../js/jquery_min_3-6-0.js"></script>
