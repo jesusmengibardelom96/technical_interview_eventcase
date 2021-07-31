a1df1b2e596fe2944bd92c0c7bf5976cf920e9ad
jesusmengibardelom96 <jesusmengibardelom96@gmail.com>
2021-07-31 12:09:17 +0200

    Password forget code

---
+++ includes/password.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include "head.php";
+?>
+<div id="login-body">
+    <div id="login-bg-image">
+        <div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="eMail *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="New password *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Repeat password *" value="" />
+                        </div>
+                        <div id="button-group" class="form-group">
+                            <input type="submit" class="btnSubmit" value="Save" />
+                        </div>
+                        <div id="link-group" class="form-group">
+                            <a href="register.php" class="ForgetPwd">Not registered yet?</a>
+                            <span> | </span>
+                            <a href="login.php" class="ForgetPwd">Go to login</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
