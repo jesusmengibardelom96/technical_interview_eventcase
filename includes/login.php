3333ce0f90d081e7464b7429b72ff85cfccb107e
jesusmengibardelom96 <jesusmengibardelom96@gmail.com>
2021-07-31 11:59:57 +0200

    Login and register screen style with bg image

--- includes/login.php
+++ includes/login.php
@@ -2,27 +2,30 @@
 session_start();
 include "head.php";
 ?>
-
-<div class="container login-container">
-    <div class="row">
-        <div class="col-md-6 login-form-1">
-            <h3>Login</h3>
-            <form>
-                <div class="form-group">
-                    <input type="email" class="form-control" placeholder="eMail *" value="" />
+<div id="login-body">
+    <div id="login-bg-image">
+        <div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-1">
+                    <h3>Login</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="eMail *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password *" value="" />
+                        </div>
+                        <div id="button-group" class="form-group">
+                            <input type="submit" class="btnSubmit" value="Login" />
+                        </div>
+                        <div id="link-group" class="form-group">
+                            <a href="register.php" class="ForgetPwd">Not registered yet?</a>
+                            <span> | </span>
+                            <a href="password.php" class="ForgetPwd">Forget Password?</a>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password *" value="" />
-                </div>
-                <div id="button-group" class="form-group">
-                    <input type="submit" class="btnSubmit" value="Login" />
-                </div>
-                <div id="link-group" class="form-group">
-                    <a href="register.php" class="ForgetPwd">Not registered yet?</a>
-                    <span> | </span>
-                    <a href="password.php" class="ForgetPwd">Forget Password?</a>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
--- includes/register.php
+++ includes/register.php
@@ -1,38 +1,41 @@
-<?php 
+<?php
 session_start();
 include "head.php";
 ?>
-
-<div class="container login-container">
-    <div class="row">
-        <div class="col-md-6 login-form-2">
-            <h3>Register</h3>
-            <form>
-                <div class="form-group">
-                    <input type="email" class="form-control" placeholder="eMail *" value="" />
+<div id="login-body">
+    <div id="login-bg-image">
+        <div class="container login-container">
+            <div class="row">
+                <div class="col-md-6 login-form-2">
+                    <h3>Register</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="email" class="form-control" placeholder="eMail *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Name *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Delivery adress *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="number" class="form-control" placeholder="Telephone *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="date" class="form-control" placeholder="Birthday date *" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Password *" value="" />
+                        </div>
+                        <div id="button-group" class="form-group">
+                            <input type="submit" class="btnSubmit" value="Register" />
+                        </div>
+                        <div id="link-group" class="form-group">
+                            <a href="login.php" class="ForgetPwd">You have an account?</a>
+                        </div>
+                    </form>
                 </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Name *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Delivery adress *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="number" class="form-control" placeholder="Telephone *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="date" class="form-control" placeholder="Birthday date *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password *" value="" />
-                </div>
-                <div class="form-group">
-                    <input type="submit" class="btnSubmit" value="Register" />
-                </div>
-                <div class="form-group">
-                    <a href="login.php" class="ForgetPwd">You have an account?</a>
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
